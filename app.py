810aec729c236bc3c08cda8d31f6582c9fe9b555
uyen1103 <nguyenuyen2017tn@gmail.com>
2025-08-29 22:02:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello from Docker!"
+def hello():
+    return "Xin chào! Đây là demo Docker từ nhóm 5 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
